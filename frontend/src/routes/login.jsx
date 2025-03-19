dc8b1029fbaa7185513065cb3b42649f847271be
WHJang-0421 <morrranii@gmail.com>
2025-03-19 08:14:54 +0000

    Add login form

--- frontend/src/routes/login.jsx
+++ frontend/src/routes/login.jsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { LoginForm } from "@/components/login-form";
 
 export const Route = createFileRoute("/login")({
   component: Login,
 });
 
 function Login() {
-  return <div>Hello "/login"!</div>;
+  return (
+    <div className="flex grow flex-col">
+      <LoginForm className="flex grow flex-col justify-center" />
+    </div>
+  );
 }
